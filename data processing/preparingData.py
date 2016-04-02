06d278517de5ecb9a7fef7fad908b9518fb09bfd
Angela Ambroz <angela.ambroz@gmail.com>
2016-04-01 21:30:02 -0400

    First output of 4 texts - huge! 31MB!

--- data processing/preparingData.py
+++ data processing/preparingData.py
@@ -30,7 +30,8 @@ MORESTOP = [
 			]
 STOP.extend(MORESTOP)
 
-def tokenizeParas(rawfile):
+
+def paraTokenizer(rawfile):
 	print "Now tokenizing into paragraphs..."
 	paras = unidecode(raw).splitlines()
 	para_index = []
@@ -62,63 +63,97 @@ def topCheck(topWords, words):
 			return 0
 
 
-with io.open(DIR + '/raw/frankenstein.txt', 'r', encoding='utf-8') as r:
-	raw = r.read()
-
-
-
-print "Word tokenizing entire text to get top words..."
-words = nltk.word_tokenize(raw)
-words = [w.lower() for w in words if w not in MORESTOP]
-vocab = sorted(set(words))
-fdist = nltk.FreqDist(words)
-top_words = [(x, y) for (x,y) in fdist.most_common(100) if x not in STOP]
-
-print "Finished tokenizing.\n"
-# print len(vocab)
-print "The top words are:"
-print top_words
-print "\n"
-
-# Paragraph indexes
-paragraphs = tokenizeParas(raw)
-
-# Overall dict
-textDict = {
-	'title': '',
-	'author': '',
-	'wordcount': len(words),
-	'paracount': len(paragraphs),
-	'vocab': len(vocab),
-	'top_words': [x for (x, y) in top_words]
-}
-
-textList = []
-
-for i, para in enumerate(paragraphs[:200]):
-	# print "Now doing para " + `i`
-	words = nltk.word_tokenize(para)
-	sentences = nltk.sent_tokenize(para)
-
-	paraDict = {}
-	paraDict = {
-		'index': i,
-		'rawText': para,
-		'words': [{'word': x, 'length': len(x)} for x in words],
-		'sentences': [{'sent': sentence, 'length': len(sentence)} for sentence in sentences],
-		'top': topCheck(top_words, words),
-		'length': len(para)
-		}
-
-	textList.append(paraDict)
-
-
-# textDict['text'] = textList
-
-pp = pprint.PrettyPrinter(indent=4)
-# pp.pprint(textDict)
-
-
-
+def overallTokenizing(rawFile):
+	print "Word tokenizing entire text to get top words..."
+	words = nltk.word_tokenize(rawFile)
+	words = [w.lower() for w in words if w not in MORESTOP]
+	vocab = sorted(set(words))
+	fdist = nltk.FreqDist(words)
+	top_words = [(x, y) for (x,y) in fdist.most_common(100) if x not in STOP]
 	
+	print "Finished tokenizing."
+	return words, vocab, top_words
+
+
+def TitleAuthor(paras):
+	# Find title, author
+	# I assume title, author etc are in the first 20 paras
+
+	print "Now pulling title and author."
+
+	for x in paragraphs[:20]:
+		if "Title" in x:
+			title = [w for w in x.split() if w != "Title:"]
+			title = ' '.join(title)
+		if "Author" in x:
+			author = [w for w in x.split() if w != "Author:"]
+			author = ' '.join(author)
+
+	return title, author
+
+
+def buildData(titleText, authorName, allWords, textVocab, topWords, paras):
+	print "Now building data..."
+
+	# Overall dict
+	textDict = {
+		'title': titleText,
+		'author': authorName,
+		'wordcount': len(allWords),
+		'paracount': len(paragraphs),
+		'vocab': len(textVocab),
+		'top_words': [x for (x, y) in topWords]
+	}
+
+	# Make the textList
+	textList = []
+
+	for i, para in enumerate(paragraphs):
+		# print "Now doing para " + `i`
+		words = nltk.word_tokenize(para)
+		sentences = nltk.sent_tokenize(para)
+
+		paraDict = {}
+		paraDict = {
+			'index': i,
+			'rawText': para,
+			'words': [{'word': x, 'length': len(x)} for x in words],
+			'sentences': [{'sent': sentence, 'length': len(sentence)} for sentence in sentences],
+			'top': topCheck(top_words, words),
+			'length': len(para)
+			}
+
+		textList.append(paraDict)
+	
+	print "The number of paras taken is " + `len(textList)`
+
+	textDict['text'] = textList
+
+	print "Data built."
+	return textDict
+
+
+# Run everything
+books = os.listdir(DIR + "/raw")
+bigJson = []
+
+for txt in books:
+	with io.open(DIR + "/raw/" + txt, 'r', encoding='utf-8') as r:		
+		raw = r.read()
+
+	print "\nNow doing: " + txt
+
+	words, vocab, top_words = overallTokenizing(raw)
+	paragraphs = paraTokenizer(raw)
+	title, author = TitleAuthor(paragraphs)
+	bookData = buildData(title, author, words, vocab, top_words, paragraphs)
+
+	bigJson.append(bookData)
+
+
+# pp.pprint(bigJson)
+# print len(bigJson)
+
+with open(DIR + "/processed/data.json", "w") as f:
+	json.dump(bigJson, f)
 
